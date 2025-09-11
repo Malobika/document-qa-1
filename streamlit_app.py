091bdbb9b71792b3fafccd71c8c113618edb619e
Malobika Roy <malobikaroy0001@gmail.com>
2025-09-11 15:58:12 +0000

    ch kj

--- streamlit_app.py
+++ streamlit_app.py
@@ -176,27 +176,7 @@ def document_qa_lab3(page_name: str):
             chat_msg =st.chat_message(message["role"])
             chat_msg.write(message["content"])
 
-        '''if promt.lower() in ["yes", "y", "yeah", "ok", "sure"]:
-                    # Use OpenAI again to provide more detail
-                    stream = client.chat.completions.create(
-                        model=model_to_use,
-                        messages=[
-                            {"role": "system", "content": "Give more detail, but still explain in very simple language like to a 10-year-old."},
-                            {"role": "user", "content": st.session_state.messages[-2]["content"]}  # the last question asked
-                        ],
-                        stream=True
-                    )
-                    with st.chat_message("assistant"):
-                        response = st.write_stream(stream)
-                    st.session_state.messages.append({"role": "assistant", "content": response})
-
-                    # Ask again
-                    more_q = "Do you want more info?"
-                    with st.chat_message("assistant"):
-                        st.write(more_q)
-                    st.session_state.messages.append({"role": "assistant", "content": more_q})
-
-                    st.session_state["mode"] = "waiting_more_info"'''
+        
 
         
         #react to user input
@@ -225,6 +205,10 @@ def document_qa_lab3(page_name: str):
             )
             with st.chat_message("assistant"):
                 response = st.write_stream(stream)
+
+            # save assistant response
+            st.session_state.messages.append({"role": "assistant", "content": response})
+
 
             # save assistant response
             more_q = "Do you want more info?"
